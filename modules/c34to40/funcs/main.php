f098954b02cd3da39936290c1eea65248de976b9
hongoctrien <hongoctrien@vinades.vn>
2014-09-19 09:22:15 +0700

    Delete php close tag

--- modules/c34to40/funcs/main.php
+++ modules/c34to40/funcs/main.php
@@ -417,6 +417,9 @@ function nv_fomat_dir( $dirname, &$contents )
 
 				// Loại bỏ khoảng trắng ()
 				$output_data = preg_replace( '/\([\s]+\)/', '()', $output_data );
+				
+				// Xóa thẻ đóng php
+				$output_data = preg_replace( '/(\s*\?>\s*)\z$/', '', $output_data );
 
 				if( $output_data != $contents_file )
 				{
