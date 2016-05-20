baa0b4e70fe401d17e6509aeb09fc8b29f325d7f
Phan Tan Dung <phantandung92@gmail.com>
2016-05-20 15:24:02 +0700

    Cập nhật tool nâng cấp các dữ liệu khác

--- modules/c34to40/funcs/other_db.php
+++ modules/c34to40/funcs/other_db.php
@@ -9,7 +9,8 @@
  * @Createdate 31/05/2010, 00:36
  */
 
-if (!defined('NV_IS_MOD_C34TO40')) die('Stop!!!');
+if (!defined('NV_IS_MOD_C34TO40'))
+    die('Stop!!!');
 
 $xtpl = new XTemplate($op . '.tpl', NV_ROOTDIR . '/themes/' . $module_info['template'] . '/modules/' . $module_file);
 $xtpl->assign('LANG', $lang_module);
@@ -21,7 +22,9 @@ $xtpl->assign('OP', $op);
 
 if ($nv_Request->isset_request('save', 'post')) {
     other();
-    //del_nv3();
+    if ($nv_Request->get_int('deletenv3', 'post', 0) == 1) {
+        del_nv3();
+    }
     $xtpl->assign('ERR', 'Thực hiện xong!');
 }
 $xtpl->parse('main');
@@ -39,17 +42,17 @@ function other()
         // bảng nv3_vi_counter
         $_sql = 'SELECT c_type, c_val, last_update, c_count, vi_count FROM ' . NV4_PREFIX . '_counter';
         $_query = $db->query($_sql);
-        
+
         while ($row = $_query->fetch()) {
             $nv_Cache->delAll();
             $sqlnv3 = 'SELECT c_type, c_val, c_count, last_update FROM ' . NV3_PREFIX . '_' . NV_LANG_DATA . '_counter';
             $result = $db->query($sqlnv3);
-            
+
             while ($row3 = $result->fetch()) {
                 if ($row['c_type'] == $row3['c_type'] and $row['c_val'] == $row3['c_val']) {
                     $row['c_count'] = $row3['c_count'];
                     $row['last_update'] = $row3['last_update'];
-                    
+
                     $sql_up = 'UPDATE ' . NV4_PREFIX . '_counter SET
 					last_update=' . $db->quote($row['last_update']) . ',
 					c_count=' . $db->quote($row['c_count']) . ',
@@ -59,33 +62,33 @@ function other()
                 }
             }
         }
-        
+
         // bảng nv3_vi_searchkeys
         $db->query('TRUNCATE ' . NV4_PREFIX . '_' . NV_LANG_DATA . '_searchkeys');
         $_sql = 'SELECT `id`, `keys`, `total`, `search_engine` FROM `' . NV3_PREFIX . '_' . NV_LANG_DATA . '_searchkeys`';
         $_query = $db->query($_sql);
-        
+
         while ($row = $_query->fetch()) {
-            
+
             $sql = "INSERT INTO " . NV4_PREFIX . '_' . NV_LANG_DATA . "_searchkeys(id, `skey`, total, search_engine)
 		      VALUES (:id,:skey,:total,:search_engine)";
-            
+
             $data_insert = array();
             $data_insert['id'] = $row['id'];
             $data_insert['skey'] = $row['keys'];
             $data_insert['total'] = $row['total'];
             $data_insert['search_engine'] = $row['search_engine'];
-            
+
             $id = intval($db->insert_id($sql, 'id', $data_insert));
         }
-        
+
         // bảng nv3_vi_referer_stats
         $db->query('TRUNCATE ' . NV4_PREFIX . '_' . NV_LANG_DATA . '_referer_stats');
         $_sql = 'SELECT `host`, `total`, `month01`, `month02`, `month03`, `month04`, `month05`, `month06`, `month07`, `month08`, `month09`, `month10`, `month11`, `month12`, `last_update` FROM `' . NV3_PREFIX . '_' . NV_LANG_DATA . '_referer_stats`';
         $_query = $db->query($_sql);
-        
+
         while ($row = $_query->fetch()) {
-            
+
             $sql = "INSERT INTO " . NV4_PREFIX . '_' . NV_LANG_DATA . "_referer_stats
 		(host, total, month01, month02, month03, month04,
 		month05, month06, month07, month08, month09, month10,
@@ -108,7 +111,8 @@ function other()
 		" . $db->quote($row['last_update']) . ")";
             $db->query($sql);
         }
-    } catch (PDOException $e) {
+    }
+    catch (PDOException $e) {
         trigger_error($e->getMessage());
     }
 }
